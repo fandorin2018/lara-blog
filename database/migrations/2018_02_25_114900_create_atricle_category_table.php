b85d5aee5c1bac001ac12308cb49de99fd331904
Vlad <choliy.vlad@gmail.com>
2018-02-25 23:04:17 +0200

    Настройка

---
+++ database/migrations/2018_02_25_114900_create_atricle_category_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAtricleCategoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('article_category', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id');
+            $table->integer('article_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('article_category');
+    }
+}
